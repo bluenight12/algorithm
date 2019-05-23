ccd04838446354ef0d5e8ebdce385f15ed8ea306
bluenight12 <ekehd258@gmail.com>
2019-05-23 21:47:15 +0900

    0

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+int input;
 int main() {
-    cout << "hi";
+    int count = 0;
+    while (1) {
+        cin >> input;
+        cout << input;
+        if (++count == 3)
+            break;
+    }
 }
